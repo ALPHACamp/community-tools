c2d1ec51c0ae09874e9cdbac3e2122ae79d686e2
Lois Chen <a8283626@gmail.com>
2023-04-17 01:21:41 +0800

    feat: add Events.MessageReactionAdd

--- src/app.js
+++ src/app.js
@@ -3,11 +3,22 @@ const dotenv = require('dotenv');
 dotenv.config();
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const token = process.env.DISCORD_TOKEN;
+const {
+  Client,
+  Collection,
+  GatewayIntentBits,
+  Events,
+  EmbedBuilder,
+  Partials
+} = require('discord.js');
 
 const client = new Client({
-  intents: [GatewayIntentBits.Guilds]
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions
+  ],
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction]
 });
 
 client.commands = new Collection();
@@ -27,5 +38,40 @@ for (const file of eventFiles) {
   }
 }
 
+client.on(Events.MessageReactionAdd, async (reaction, user) => {
+  // When a reaction is received, check if the structure is partial
+  if (reaction.partial) {
+    // If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+    try {
+      await reaction.fetch();
+    } catch (error) {
+      console.error('Something went wrong when fetching the message:', error);
+      // Return as `reaction.message.author` may be undefined/null
+      return;
+    }
+  }
+
+  if (reaction.emoji.name === '🧡' && reaction.message.author !== user) {
+    const embedMessage = new EmbedBuilder()
+      .setColor('#FFCC39')
+      .setDescription(
+        `太棒了！ 有人覺得這則在 <#${reaction.message.channelId}> 的 [訊息](https://discord.com/channels/${reaction.message.guildId}/${reaction.message.channelId}/${reaction.message.id}) 有幫助，並且送上了一個 🧡`
+      )
+      .addFields({ name: 'From', value: `${user}`, inline: true })
+      .addFields({
+        name: 'To',
+        value: `${reaction.message.author}`,
+        inline: true
+      })
+      .addFields({
+        name: 'Total 🧡',
+        value: `${reaction.count}`,
+        inline: true
+      });
+    const channel = client.channels.cache.get(process.env.CHANNEL_ID);
+    channel.send({ embeds: [embedMessage] });
+  }
+});
+
 // Log in to Discord with your client's token
-client.login(token);
+client.login(process.env.DISCORD_TOKEN);
